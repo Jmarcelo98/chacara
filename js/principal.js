bbae4403003ddca56172876d93cf30ab0045b381
Joao Marcelo <joaomarcelo588@gmail.com>
2019-12-26 17:04:35 -0300

    Bastantes modificações

---
+++ js/principal.js
@@ -0,0 +1,11 @@
+//VOLTAR AO TOPO
+// $(window).on('scroll', function () {
+//     var wintop = $(this).scrollTop();
+
+//     if (wintop >= $('#iddareferencia').offset().top) {
+//         $('#iddobotao').fadeIn("slow");
+//     } else {
+//         $('#idobotao').fadeOut("slow");
+//     }
+// })
+
